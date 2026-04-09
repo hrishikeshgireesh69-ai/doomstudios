1f2bc7465dfd0b5444bd39db9f812868f0d805b3
hrishikeshdoom <hrishikeshgireesh69@gmail.com>
2026-04-09 16:21:29 +0530

    Remove contact information from Footer component
    Removed contact details from the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -71,8 +71,6 @@ export default function Footer() {
           <div>
             <h4 className="text-white font-semibold mb-4">Contact</h4>
             <ul className="space-y-2 text-gray-400 text-sm">
-              <li>Dubai, UAE</li>
-              <li>+63 9167327740</li>
               <li>doomstudiosofficial@gmail.com</li>
             </ul>
           </div>
